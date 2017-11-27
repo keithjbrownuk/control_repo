e4d6efb12557d6aaf1b9e45d73b73d6f739baa26
keithjbrownuk <33933285+keithjbrownuk@users.noreply.github.com>
2017-11-27 20:44:22 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,9 +2,13 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 
-node / ^web/ {
+node /^web/ {
   include role::app_server
 }
 
